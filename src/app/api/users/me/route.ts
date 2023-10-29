4e68d8fce5319b54da67f0f49525ef50251c6b2d
Sahil <sk2003311@gmail.com>
2023-10-29 16:47:33 +0530

    refactor the profile/[id] page

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -9,7 +9,7 @@ connect()
 export async function GET(request:NextRequest) {
     try {
         const userId = await getDataFromToken(request);
-        const user = await User.findOne({_id: userId}).select("-password")
+        const user = await User.findOne({_id: userId})  //.select("-password")
         return NextResponse.json({
             message: "User found",
             data: user,
--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,9 +1,41 @@
-export default function UserProfile({params}: any) {
+"use client"
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function UserProfile({ params }: any) {
+    const [userData, setUserData] = useState({
+        email: "",
+        username: "",
+        _id: "",
+        password: "",
+    });
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await axios.get("/api/users/me");
+                setUserData(res.data.data);
+    
+            } catch (error:any) {
+                console.log("API Error:", error.message);
+            }
+        };
+
+        fetchData();
+    }, []);
+
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1>Profile Details</h1>
+            <h1 className="text-primary">Profile Details</h1>
             <hr />
-            <p className="bg-amber-500">Profile page {params.id}</p>
+
+            
+            <div className="w-[30rem] p-[1rem] btn-primary">
+                <p >Email: <span className="underline underline-offset-2">{userData.email}</span></p>
+                <p >Username: <span className="underline underline-offset-2">{userData.username}</span></p>
+                <p >ID: <span className="underline underline-offset-2">{userData._id}</span></p>
+                <p className="truncate">Hashed Password: <span className="underline underline-offset-2">{userData.password}</span></p>
+            </div>
         </div>
-    )
+    );
 }
