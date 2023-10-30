813c21e8060e37fe7c5f36b2965f22996bd4a1ad
Sahil <sk2003311@gmail.com>
2023-10-30 20:33:11 +0530

    added the loader component in forgot pass pages

--- src/app/forgotpassword/page.tsx
+++ src/app/forgotpassword/page.tsx
@@ -3,9 +3,11 @@
 import React from 'react'
 import { useRouter } from "next/navigation";
 import  axios  from "axios";
+import Loader from "@/components/Loader";
 
 export default function ForgotPasswordPage() {
 
+  const [loading, setLoading] = React.useState(false);
   const [email, setEmail] = React.useState("")
   const [isButtonDisabled, setIsButtonDisabled] = React.useState(true);
 
@@ -24,17 +26,23 @@ export default function ForgotPasswordPage() {
     // For now, let's just log the email
     
     try{
-
+      setLoading(true)
       const response = await axios.post("/api/users/forgotpassword", { email: email })
       console.log("mail send successfully", response.data)
     } catch(error:any){
       console.log("mail send Unsuccessfully", error.message)
     }
+    finally {
+      setLoading(false)
+    }
 
   }
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2 text-slate-950">
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 text-slate-950 relative">
+
+        {loading && <Loader/>}
+
         <h1 className='text-primary'>Submit your mail</h1>
         <p className='text-sec'>we'll send you a link to change your password</p>
 
--- src/app/setforgotpass/page.tsx
+++ src/app/setforgotpass/page.tsx
@@ -2,6 +2,7 @@
 import axios from 'axios'
 import React from 'react'
 import { useRouter } from "next/navigation";
+import Loader from "@/components/Loader";
 
 export default function SetForgotPassPage() {
 
@@ -9,6 +10,7 @@ export default function SetForgotPassPage() {
     const[token, setToken] = React.useState("")
     const[pass, setPass] = React.useState("")
     const[confirm, setConfirm] = React.useState("")
+    const [loading, setLoading] = React.useState(false);
 
     const [isButtonDisabled, setIsButtonDisabled] = React.useState(true);
 
@@ -34,17 +36,22 @@ export default function SetForgotPassPage() {
       }
     
       try {
+        setLoading(true)
         await axios.post("api/users/setforgotpass", { token, pass });
         console.log("Password Changed Succesfully")
         router.push("/login")
       } catch (error: any) {
         console.log('Error:', error.message);
       }
+      finally {
+        setLoading(false)
+      }
     };
     
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2 text-slate-950">
+    <div className="flex flex-col items-center justify-center min-h-screen py-2 text-slate-950 relative">
+      {loading && <Loader/>}
         <h1 className='text-primary'>Set New Password</h1>
 
         <form action="" className='flex flex-col my-4 items-center'>
